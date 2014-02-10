aff955de2211bb3eb4691fe73a9407cb080e973d
h-hirokawa <h.hirokawa@realglobe.jp>
2014-02-10 20:09:57 +0900

    Initial commit.

---
+++ init.el
@@ -0,0 +1,39 @@
+;;; init.el --- My init.el.
+
+;;; Commentary:
+
+;;; Code:
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+(require 'pallet)
+
+(load "term/bobcat")
+(defun my-keyboard-translations (&optional frame)
+  "Not to forget bobcat whenever the new frame is created.
+FRAME is an Emacs frame"
+  (when (fboundp 'terminal-init-bobcat)
+    (terminal-init-bobcat)))
+(my-keyboard-translations)
+(add-hook 'after-make-frame-functions 'my-keyboard-translations)
+
+(add-hook 'find-file-hook 'flycheck-mode)
+
+(defun coffee-custom ()
+  "Coffee-mode-hook."
+  (setq tab-width 2)
+)
+(add-hook 'coffee-mode-hook '(lambda () (coffee-custom)))
+
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.ctp$" . php-mode))
+
+(add-hook 'python-mode-hook 'auto-complete-mode)
+(add-hook 'python-mode-hook 'jedi:setup)
+(defvar jedi:setup-keys t)
+(defvar jedi:complete-on-dot t)
+
+(setq backup-directory-alist `(("." . "~/.emacs.d/.saves")))
+(setq-default indent-tabs-mode nil)
+
+(provide 'init)
+;;; init.el ends here
